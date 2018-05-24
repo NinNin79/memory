78135c4f2ed9e02c3ee5490c8c3ed9290a437ee7
Alain FOURNIER <alfo@ouest.sesame.infotel.com>
2018-05-24 13:56:21 +0200

    Reagissez aux évènnements

--- src/Card.js
+++ src/Card.js
@@ -4,13 +4,17 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback }) => (
-  <div className={`card ${feedback}`}>
+const Card = ({ card, feedback, onClick }) => (
+  <div className={`card ${feedback}`} onClick = {() => handleCardClick(card)}>
     <span className="symbol">
       {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
     </span>
   </div>
 )
+
+function handleCardClick(card) {
+  console.log(card, 'clicked')
+}
 
 export default Card
 
